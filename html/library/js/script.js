00aff11df53c03119ccf44fbaed7df55c98e59a4
Matthew Ell <ell.matthew@gmail.com>
2013-03-28 08:33:48 -0400

    Directory Clean up
    Added html folder to theme directory

---
+++ html/library/js/script.js
@@ -0,0 +1,27 @@
+/* Author: Matthew Ell
+
+*/
+
+$('li:first-child, tr:first-child, td:first-child').addClass('first-child');
+$('li:last-child, tr:last-child, td:last-child').addClass('last-child');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
