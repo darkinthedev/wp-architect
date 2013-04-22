8867faebd2e032c624213f344c73c47aaadd5161
Matthew Ell <ell.matthew@gmail.com>
2013-04-22 12:56:55 -0400

    Fix code error
    Signed-off-by: Matthew Ell <ell.matthew@gmail.com>

--- 404.php
+++ 404.php
@@ -14,34 +14,17 @@ get_header(); ?>
 			<article id="post-0" class="post error404 not-found">
 				<header class="entry-header">
 					<h1 class="entry-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'wp_arch' ); ?></h1>
-				</header><?php .entry-header ?>
+				</header><?php //.entry-header ?>
 
 				<div class="entry-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'wp_arch' ); ?></p>
 
 					<?php get_search_form(); ?>
 
-					<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+				</div><?php //.entry-content ?>
+			</article><?php //#post-0 .post .error404 .not-found ?>
 
-					<div class="widget">
-						<h2 class="widgettitle"><?php _e( 'Most Used Categories', 'wp_arch' ); ?></h2>
-						<ul>
-						<?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
-						</ul>
-					</div><?php .widget ?>
-
-					<?php
-					/* translators: %1$s: smilie */
-					$archive_content = '<p>' . sprintf( __( 'Try looking in the monthly archives. %1$s', 'wp_arch' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-					?>
-
-					<?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-
-				</div><?php .entry-content ?>
-			</article><?php #post-0 .post .error404 .not-found ?>
-
-		</div><?php #content .site-content ?>
-	</div><?php #primary .content-area ?>
+		</div><?php //#content .site-content ?>
+	</div><?php //#primary .content-area ?>
 
 <?php get_footer(); ?>
