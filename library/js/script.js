d7da014f1cc0392432cacbb31d73069cc6a2a894
Matthew Ell <ell.matthew@gmail.com>
2013-04-25 08:16:28 -0400

    Standardizing Refinements

--- library/js/script.js
+++ library/js/script.js
@@ -1,10 +1,14 @@
 /* Author: 
 
 */
+// Stuff to do as soon as the DOM is ready;
+$(document).ready(function() {
+    
+    //Add First and Last Child Class to List, Rows and Cells items
+    $("li:first-child, tr:first-child, td:first-child").addClass('first-child');
+    $("li:last-child, tr:last-child, td:last-child").addClass('last-child');
 
-//Add First and Last Child Class to List, Rows and Cells items
-$("li:first-child, tr:first-child, td:first-child").addClass('first-child');
-$("li:last-child, tr:last-child, td:last-child").addClass('last-child');
+});
 
 
 
