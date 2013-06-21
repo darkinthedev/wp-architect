cb1e625370f1e23f1ac1ad8f9439971514406234
Matthew Ell <ell.matthew@gmail.com>
2013-06-21 16:39:28 -0400

    Tweaks

---
+++ page-custom.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ *   Template Name: My Custom Page
+ *   Description: A Page Template with a darker design.
+ *   Codex: http://codex.wordpress.org/Page_Templates#Custom_Page_Template
+ *
+ *   @package wp_arch
+ *   @since wp_arch 1.0
+ *
+ */
+
+
+?>
