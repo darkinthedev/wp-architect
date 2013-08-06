44611890dbdc45bb73942555ef3ccd2257bcb20e
Matthew Ell <ell.matthew@gmail.com>
2013-08-06 15:51:15 -0400

    add custom post type example code

--- functions.php
+++ functions.php
@@ -449,4 +449,35 @@ function my_em_disable_gallery(){
 }
 add_action('init', 'my_em_disable_gallery');
 
+ // Custom Post Types 
+// add_action('init', 'wp_arch_cpts');
+// function wp_arch_cpts() { 
+//     // Testimonials
+//     register_post_type( 'wp_arch_testimonial', 
+//         array(  
+//             'labels' => array(
+//                 'name' => _x('Testimonials', 'post type general name'),
+//                 'singular_name' => _x('Testimonial', 'post type singular name'),
+//                 'add_new' => _x('Add New', 'testimonial'),
+//                 'add_new_item' => __('Add New Testimonial'),
+//                 'edit_item' => __('Edit Testimonial'),
+//                 'new_item' => __('New Testimonial'),
+//                 'view_item' => __('View Testimonial'),
+//                 'search_items' => __('Search Testimonial'),
+//                 'not_found' =>  __('No testimonials found'),
+//                 'not_found_in_trash' => __('No testimonials found in Trash'), 
+//                 'parent_item_colon' => ''
+//             ),
+//             'public' => true,  
+//             'description'  => 'Nationwide Security Testimonials',
+//             'exclude_from_search' => true,
+//             'hierarchical' => true,
+//             'supports' => array(  
+//                 'title',  
+//                 'editor'
+//             ),
+//         )
+//     );
+// } 
+
  ?>
