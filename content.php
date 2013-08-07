aea1a4805af563c37a8c0c1a8b4fe0ba96f1fc6c
Matthew Ell <ell.matthew@gmail.com>
2013-08-07 14:28:22 -0400

    - update to starter styles. Added default  form style

--- content.php
+++ content.php
@@ -8,7 +8,6 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'wp_arch' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php wp_arch_posted_on(); ?>
