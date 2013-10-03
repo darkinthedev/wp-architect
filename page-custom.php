308d3c47bc44fc47aaefc50a6fefd76ee46225ae
Matthew Ell <ell.matthew@gmail.com>
2013-10-03 07:52:53 -0400

    Update page-custom.php

--- page-custom.php
+++ page-custom.php
@@ -1,14 +1,13 @@
 <?php
 
-/*
- *   Template Name: My Custom Page
- *   Description: A Page Template with a darker design.
- *   Codex: http://codex.wordpress.org/Page_Templates#Custom_Page_Template
+/**
+ *Template Name: My Custom Page
+ *Description: A Page Template with a darker design.
+ *Codex: http://codex.wordpress.org/Page_Templates#Custom_Page_Template
  *
- *   @package wp_arch
- *   @since wp_arch 1.0
+ *@package wp_arch
+ *@since wp_arch 1.0
  *
  */
-
 
 ?>
