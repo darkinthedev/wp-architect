d5f41c10a0851b9da16cdf66f6e649885b1dd9f5
Matthew Ell <ell.matthew@gmail.com>
2013-11-27 13:02:05 -0500

    Added Images path for relative paths

--- config.rb
+++ config.rb
@@ -1,6 +1,15 @@
-# TODO Add Images path for relative paths
+# Load in Susy Grid Layout Framework
 require 'susy'
-css_dir = 'library/build/css/'
-sass_dir = 'library/scss/'
-javascripts_dir = 'library/js/'
+
+# Basic Configurations
+css_dir = 'library/build/css'
+sass_dir = 'library/scss'
+javascripts_dir = 'library/js'
 output_style = :compressed
+
+## Image Handeling 
+## Use the function for output in SASS file: image-url('file_name_here.gif')
+#Sets image directory
+images_dir = '/library/img'
+#Sets output URL to be reletive to image directory
+relative_assets = true
