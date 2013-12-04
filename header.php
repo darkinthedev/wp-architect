34ccba79b5b21d54d599773a9abc68703757ad9c
Matthew Ell <ell.matthew@gmail.com>
2013-12-04 14:47:24 -0500

    Added outdated browser warning for less than IE8

--- header.php
+++ header.php
@@ -33,6 +33,9 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+   <!--[if lt IE 8]>
+    <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+   <![endif]-->
     <div class="hfeed page">
         <header role="banner">
             <hgroup>
