1bb7f4c300fb28588b9003f3b288bc7ec1b2a6eb
Matthew Ell <ell.matthew@gmail.com>
2014-01-30 07:59:24 -0500

    Archive gets excerpts; Edit link drops to own line

--- content.php
+++ content.php
@@ -6,6 +6,7 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
 	<header class="entry-header">
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'wp_arch' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 		<?php if ( 'post' == get_post_type() ) : ?>
@@ -15,18 +16,20 @@
 		<?php endif; ?>
 	</header><?php //.entry-header ?>
 
-	<?php if ( is_search() ) : // Only display Excerpts for Search ?>
-	<div class="entry-summary">
-		<?php the_excerpt(); ?>
-	</div><?php //.entry-summary ?>
-	<?php else : ?>
-	<div class="entry-content group">
-		<?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-  the_post_thumbnail('large');
-} ?>
-		<p><?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'wp_arch' ) ); ?></p>
-		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'wp_arch' ), 'after' => '</div>' ) ); ?>
-	</div><?php //.entry-content ?>
+	<?php if ( is_search() || is_archive() ) : // Only display Excerpts for Search and Archive pages ?>
+	
+		<div class="entry-summary">
+			<?php the_excerpt(); ?>
+		</div><?php //.entry-summary ?>
+	
+	<?php else : // Display full content on all other posts and pages ?>
+		<div class="entry-content group">
+			<?php if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+				the_post_thumbnail('large');
+			} ?>
+			<p><?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'wp_arch' ) ); ?></p>
+			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'wp_arch' ), 'after' => '</div>' ) ); ?>
+		</div><?php //.entry-content ?>
 	<?php endif; ?>
 
 	<footer class="entry-meta">
@@ -61,6 +64,6 @@
 		
 		<?php } ?>
 
-		<?php edit_post_link( __( 'Edit', 'wp_arch' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
+		<?php edit_post_link( __( 'Edit', 'wp_arch' ), '<br/><br/><span class="edit-link">', '</span>' ); ?>
 	</footer><?php //.entry-meta ?>
 </article><?php #post-<?php the_ID(); ?>
