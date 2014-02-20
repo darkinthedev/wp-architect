c02aaebdd97fc0f37a7f797044055f3f1a8cd3cc
Matthew Ell <ell.matthew@gmail.com>
2014-02-20 08:55:25 -0500

    Rebuild Grunt.JS; Tidy up files and directories

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,58 @@
+'use strict';
+
 module.exports = function(grunt) {
 
-  // measures the time each task takes
-  require('time-grunt')(grunt);
+    // measures the time each task takes
+    require('time-grunt')(grunt);
 
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
-  grunt.loadNpmTasks("grunt-modernizr");
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks("grunt-modernizr");
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // load all grunt tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+        // Clean Build Folder
+        clean: {
+            build: {
+                src: ["library/build"]
+            }
+        },
+
         // Uglify/Minification for JS files
         uglify: {
-          options: {
-            banner: '/*! Processed <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> */\n'
-          },
-          build: {
-            src: ['library/js/plugins.js', 'library/js/script.js', 'library/js/mylibs/*.js'],
-            dest: 'library/build/js/site.min.js'
-          }
+            options: {
+                banner: '/*! Processed <%= grunt.template.today("dddd, mmmm dS, yyyy, h:MM:ss TT") %> */\n'
+            },
+            build: {
+                src: ['library/js/plugins.js', 'library/js/script.js', 'library/js/mylibs/*.js'],
+                dest: 'library/build/js/site.min.js'
+            }
         },
 
         // CSS.min
         cssmin: {
-          minify: {
+            minify: {
             expand: true,
-            cwd: 'library/pack/normalize-css/',
+            cwd: 'library/bower_components/normalize-css/',
             src: ['*.css', '!*.min.css'],
             dest: 'library/scss/vendor/',
             ext: '.scss'
-          }
+            }
         },
 
         // Compass grunt – see config.rb
         compass: {
-          dev: {
-            options: {
-              config: 'config.rb'
+            dev: {
+                options: {
+                config: 'config.rb'
+                }
             }
-          }
         },
 
         // Image Minification 
@@ -61,82 +70,88 @@ module.exports = function(grunt) {
         // Modernizr Grunt - custom modernizr build. 
         modernizr: {
 
-          dist: {
+            dist: {
 
-            // [REQUIRED] Path to the build you're using for development.
-            "devFile" : "library/pack/modernizr/modernizr.js",
+                // [REQUIRED] Path to the build you're using for development.
+                "devFile" : "library/bower_components/modernizr/modernizr.js",
 
-            // [REQUIRED] Path to save out the built file.
-            "outputFile" : "library/build/js/libs/modernizr-custom.js",
+                // [REQUIRED] Path to save out the built file.
+                "outputFile" : "library/build/js/libs/modernizr-custom.js",
 
-            // Based on default settings on http://modernizr.com/download/
-            "extra" : {
-                "shiv" : true,
-                "printshiv" : false,
-                "load" : true,
-                "mq" : false,
-                "cssclasses" : true
-            },
+                // Based on default settings on http://modernizr.com/download/
+                "extra" : {
+                    "shiv" : true,
+                    "printshiv" : false,
+                    "load" : true,
+                    "mq" : false,
+                    "cssclasses" : true
+                },
 
-            // Based on default settings on http://modernizr.com/download/
-            "extensibility" : {
-                "addtest" : false,
-                "prefixed" : false,
-                "teststyles" : false,
-                "testprops" : false,
-                "testallprops" : false,
-                "hasevents" : false,
-                "prefixes" : false,
-                "domprefixes" : false
-            },
+                // Based on default settings on http://modernizr.com/download/
+                "extensibility" : {
+                    "addtest" : false,
+                    "prefixed" : false,
+                    "teststyles" : false,
+                    "testprops" : false,
+                    "testallprops" : false,
+                    "hasevents" : false,
+                    "prefixes" : false,
+                    "domprefixes" : false
+                },
 
-            // By default, source is uglified before saving
-            "uglify" : true,
+                // By default, source is uglified before saving
+                "uglify" : true,
 
-            // By default, this task will crawl your project for references to Modernizr tests.
-            // Set to false to disable.
-            "parseFiles" : true,
+                // By default, this task will crawl your project for references to Modernizr tests.
+                // Set to false to disable.
+                "parseFiles" : true,
 
-            // When parseFiles = true, this task will crawl all *.js, *.css, *.scss files, except files that are in node_modules/.
-            // You can override this by defining a "files" array below.
-            "files" : {
-              "src" : ['library/scss/*.scss', 'library/scss/**/*.scss', 'library/js/*.js' ]
-            },
+                // When parseFiles = true, this task will crawl all *.js, *.css, *.scss files, except files that are in node_modules/.
+                // You can override this by defining a "files" array below.
+                "files" : {
+                    "src" : ['library/scss/*.scss', 'library/scss/**/*.scss', 'library/js/*.js' ]
+                },
 
-            // When parseFiles = true, matchCommunityTests = true will attempt to
-            // match user-contributed tests.
-            "matchCommunityTests" : false,
-          }
+                // When parseFiles = true, matchCommunityTests = true will attempt to
+                // match user-contributed tests.
+                "matchCommunityTests" : false,
+            }
         },
-        
+
         // Watch Task
         watch: {
-          
-          options: {
-            livereload: true
-          },
 
-          // watch for html and php files
+            options: {
+                livereload: true
+            },
+            
             html: {
-            files: ['*.html', '*.php']
-          },
-
-          // watch for scss files
-          sass: {
-            files: ['library/scss/*.scss', 'library/scss/vendor/**/*.scss', 'library/scss/vendor/*.scss'],
-            tasks: ['compass:dev'],
-          },
-
-          // watch for js files 
-          js: {
-            files: ['library/js/**/*.js'],
-            tasks: ['newer:uglify']
-          },
+                files: ['*.html', '*.php']
+            },
+            
+            compass: {
+                files: ['library/scss/*.scss', 'library/scss/vendor/**/*.scss', 'library/scss/vendor/*.scss'],
+                tasks: ['compass:dev'],
+            },
+            
+            js: {
+                files: ['library/js/**/*.js'],
+                tasks: ['newer:uglify']
+            },
         }
-  });
-  
-  // Running grunt command starts 'watch' task and waits for changes
-  grunt.registerTask('default', 'watch');
-  grunt.registerTask('imgmin', ['newer:imagemin']);
+    });
 
+    // Register Tasks
+    grunt.registerTask('default', [
+        'clean',
+        'uglify',
+        'modernizr',
+        'compass',
+        'cssmin',
+        'imagemin'
+    ]);
+
+    grunt.registerTask('dev', [
+        'watch'
+    ]);
 };
