3063911181f88b4c0b13c9314bdd93b5739d4892
Matthew Ell <ell.matthew@gmail.com>
2014-03-13 22:21:17 -0400

    Modular Enhancements

--- library/js/script.js
+++ library/js/script.js
@@ -1,12 +1,40 @@
-/* Author: 
+/* Author:
 
 */
 // Stuff to do as soon as the DOM is ready;
 $(document).ready(function() {
 
-    
+    html5();
     hooksnhelpers();
     dropDownMenu();
+
+    function html5 () {
+        //http://webdesignerwall.com/tutorials/cross-browser-html5-placeholder-text
+        if(!Modernizr.input.placeholder){
+
+            $('[placeholder]').focus(function() {
+                var input = $(this);
+                if (input.val() == input.attr('placeholder')) {
+                    input.val('');
+                    input.removeClass('placeholder');
+                }
+            }).blur(function() {
+                var input = $(this);
+                if (input.val() == '' || input.val() == input.attr('placeholder')) {
+                    input.addClass('placeholder');
+                    input.val(input.attr('placeholder'));
+                }
+                }).blur();
+                $('[placeholder]').parents('form').submit(function() {
+                    $(this).find('[placeholder]').each(function() {
+                        var input = $(this);
+                        if (input.val() == input.attr('placeholder')) {
+                            input.val('');
+                        }
+                    })
+                });
+        }
+    }
 
     function hooksnhelpers () {
         //Add First and Last Child Class to List, Rows and Cells items
@@ -46,7 +74,7 @@ $(document).ready(function() {
                $this.prev('ul.subMenu').addClass('active');
                $this.addClass('active');
             }
-        });  
+        });
     }
 
 });
